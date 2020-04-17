96cc68817cf14d20255bd8d1b144558a15ac4665
Thierry Gohier-Desjardins <thierry.gohier-desjardins.1@ulaval.ca>
2020-04-17 15:17:27 -0400

    retrait des fonctionalités de l'affichage TP3 dans le main

--- pychecs2/__main__.py
+++ pychecs2/__main__.py
@@ -3,9 +3,11 @@
 Importez les modules nécessaires et démarrez votre programme à partir d'ici. Le code fourni n'est qu'à titre d'exemple.
 
 """
+
 from pychecs2.interface.interface import Fenetre
 
 if __name__ == '__main__':
     # Création et affichage d'une fenêtre (aucun lien avec la partie ci-haut).
     f = Fenetre()
     f.mainloop()
+
